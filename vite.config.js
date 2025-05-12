594fe26f9e744c2e940e07bda576a3cd2ef2d2d4
Vaishnav <da.pressed.dev@gmail.com>
2025-05-12 17:32:14 +0530

    deleted tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
   base: "/pkmn_analyzer",
   resolve: {
     alias: {
